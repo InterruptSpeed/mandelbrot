ffead3438f8797ec79583e48fefdf6fa121a59b1
Steven Docherty <sdocherty@InterruptSpeed.com>
2018-09-04 10:06:15 -0400

    added alternate functions and tracing

--- main.go
+++ main.go
@@ -6,19 +6,26 @@ import (
 	"image/png"
 	"log"
 	"os"
+	"runtime/trace"
+	"sync"
+)
+
+const (
+	output = "out.png"
+	width  = 2048
+	height = 2048
 )
 
 func main() {
-	const output string = "./out.png"
-	const width int = 700
-	const height int = 700
+	trace.Start(os.Stdout)
+	defer trace.Stop()
 
 	f, err := os.Create(output)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	img := createSeq(width, height)
+	img := createRowWorkers(width, height)
 
 	if err = png.Encode(f, img); err != nil {
 		log.Fatal(err)
@@ -32,6 +39,92 @@ func createSeq(width, height int) image.Image {
 			m.Set(i, j, pixel(i, j, width, height))
 		}
 	}
+	return m
+}
+
+func createPixel(width, height int) image.Image {
+	m := image.NewGray(image.Rect(0, 0, width, height))
+	var w sync.WaitGroup
+	w.Add(width * height)
+	for i := 0; i < width; i++ {
+		for j := 0; j < height; j++ {
+			go func(i, j int) {
+				m.Set(i, j, pixel(i, j, width, height))
+				w.Done()
+			}(i, j)
+		}
+	}
+	w.Wait()
+	return m
+}
+
+func createRow(width, height int) image.Image {
+	m := image.NewGray(image.Rect(0, 0, width, height))
+	var w sync.WaitGroup
+	w.Add(width)
+	for i := 0; i < width; i++ {
+		go func(i int) {
+			for j := 0; j < height; j++ {
+				m.Set(i, j, pixel(i, j, width, height))
+			}
+			w.Done()
+		}(i)
+	}
+	w.Wait()
+	return m
+}
+
+func createWorkers(width, height int) image.Image {
+	m := image.NewGray(image.Rect(0, 0, width, height))
+
+	type px struct{ x, y int }
+
+	c := make(chan px, width*height)
+
+	var w sync.WaitGroup
+	w.Add(8)
+	for i := 0; i < 8; i++ {
+		go func() {
+			for px := range c {
+				m.Set(px.x, px.y, pixel(px.x, px.y, width, height))
+			}
+			w.Done()
+		}()
+	}
+	for i := 0; i < width; i++ {
+		for j := 0; j < height; j++ {
+			c <- px{i, j}
+		}
+	}
+	close(c)
+	w.Wait()
+
+	return m
+}
+
+func createRowWorkers(width, height int) image.Image {
+	m := image.NewGray(image.Rect(0, 0, width, height))
+
+	c := make(chan int, width)
+
+	var w sync.WaitGroup
+	w.Add(8)
+	for i := 0; i < 8; i++ {
+		go func() {
+			for i := range c {
+				for j := 0; j < height; j++ {
+					m.Set(i, j, pixel(i, j, width, height))
+				}
+			}
+			w.Done()
+		}()
+	}
+	for i := 0; i < width; i++ {
+		c <- i
+	}
+	close(c)
+	w.Wait()
+
 	return m
 }
 
